6be08649316765628e7abecc1a8a0f4a12435f4d
amgpdt <130062580+amgpdt@users.noreply.github.com>
2023-09-04 20:54:00 +0800

    modify and add metrics in jinja templates (#22)
    * modify and add metrics in jinja templates
    
    * add another missing file from initial commit
    
    * remove unnecessary files

--- etl_project/sql/transform/moving_averages.sql
+++ etl_project/sql/transform/moving_averages.sql
@@ -7,6 +7,8 @@ SELECT
     currency,
     base_currency,
     date,
-    AVG(rate) OVER (PARTITION BY currency ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) as moving_average
+    AVG(rate) OVER (PARTITION BY currency ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) as seven_day_moving_average,
+    AVG(rate) OVER (PARTITION BY currency ORDER BY date ROWS BETWEEN 29 PRECEDING AND CURRENT ROW) as thirty_day_moving_average,
+    AVG(rate) OVER (PARTITION BY currency ORDER BY date ROWS BETWEEN 89 PRECEDING AND CURRENT ROW) as ninety_day_moving_average
 FROM
     {{ config.source_table_name }}
